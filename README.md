57b4efea8b087fdcc811071a00ad4f07ad955ad3
Filip Karlo Došilović <filipk.dosilovic@gmail.com>
2023-07-26 14:28:17 +0200

    Add experiments and results for knn (d).

--- README.md
+++ README.md
@@ -13,18 +13,20 @@ You can see the hyperparameters in the table below.
 | :------------------------- | :----------: | :-----------: | :--------: | :------: |
 | **LogisticRegression** (D) |   $0.8844$   |   $0.8915$    |  $0.8828$  | $0.8829$ |
 | **LogisticRegression** (O) |              |               |            |          |
-| **KNN** (D)                |              |               |            |          |
+| **KNN** (D)                |   $0.7631$   |   $0.8223$    |  $0.7577$  | $0.7643$ |
 | **KNN** (O)                |              |               |            |          |
 | **Nadaraya-Watson** (D)    |              |               |            |          |
 | **Nadaraya-Watson** (O)    |              |               |            |          |
 
 **Hyperparameters**:
 
+Random seed for all experiments and models is set to $42$.
+
 | **Model**                  |                    **Hyperparameters**                    |
 | :------------------------- | :-------------------------------------------------------: |
 | **LogisticRegression** (D) | lr=0.01, wd=0.001, bs=64, epochs=10, vs=0.16666, ns=30000 |
 | **LogisticRegression** (O) |                                                           |
-| **KNN** (D)                |                                                           |
+| **KNN** (D)                |                       k=3,ns=10000                        |
 | **KNN** (O)                |                                                           |
 | **Nadaraya-Watson** (D)    |                                                           |
 | **Nadaraya-Watson** (O)    |                                                           |
---
+++ experiments/train_eval_knn.py
@@ -0,0 +1,77 @@
+"""The following script is used to train the logistic regression model and evaluate it on the test set."""
+
+import numpy as np
+
+from miniml import load_mnist
+from miniml import KNearestNeighborClassifier, NaiveNearestNeighbors
+from miniml import (
+    compute_accuracy,
+    precision_recall_fscore_support,
+)
+
+
+def parse_args():
+    import argparse
+
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument("--dataset", type=str, help="Path to the MNIST dataset.")
+    parser.add_argument("--seed", type=int, default=42)
+    parser.add_argument("--k", type=int, default=3, help="Number of neighbors.")
+    parser.add_argument("--n-samples", type=int, default=30000)
+
+    return parser.parse_args()
+
+
+def predict_batches(model, test_images, batch_size=64):
+    from tqdm import tqdm
+
+    n_batches = test_images.shape[0] // batch_size
+    if test_images.shape[0] % batch_size != 0:
+        n_batches += 1
+
+    y_pred = np.empty(test_images.shape[0])
+    for i in tqdm(range(n_batches)):
+        start = i * batch_size
+        end = min(test_images.shape[0], start + batch_size)
+        y_pred[start:end] = model.predict(test_images[start:end])
+    return y_pred
+
+
+def main(args):
+    # Load the dataset.
+    X_train, y_train = load_mnist(args.dataset, split="train")
+    X_test, y_test = load_mnist(args.dataset, split="test")
+
+    # Sample training examples for faster training.
+    np.random.seed(args.seed)
+    indices = np.random.permutation(X_train.shape[0])[: args.n_samples]
+    X_train = X_train[indices]
+    y_train = y_train[indices]
+
+    nn = NaiveNearestNeighbors(k=args.k)
+
+    # Create the model.
+    model = KNearestNeighborClassifier(nn)
+    model.fit(X_train, y_train)
+
+    # Evaluate the model.
+    y_pred = predict_batches(model, X_test)
+
+    y_test = y_test.astype(np.int64)
+    y_pred = y_pred.astype(np.int64)
+
+    accuracy = compute_accuracy(y_test, y_pred)
+    precision, recall, fscore, _ = precision_recall_fscore_support(y_test, y_pred)
+
+    print("Test set performance:\n")
+    print(f"Accuracy:   {accuracy:10.4f}")
+    print(f"Precision:  {np.mean(precision):10.4f}")
+    print(f"Recall:     {np.mean(recall):10.4f}")
+    print(f"F-score:    {np.mean(fscore):10.4f}")
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    main(args)
